01f32523fc3ebd566c0af668b0c5b29ef8a1bedc
Mario Sangiorgio <mariosangiorgio@gmail.com>
2016-11-30 17:06:51 +0000

    Typescript setup

---
+++ extension.ts
@@ -0,0 +1,4 @@
+import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+export function activate(ctx: ExtensionContext) {
+    console.log('ledger is now active!');
+}
