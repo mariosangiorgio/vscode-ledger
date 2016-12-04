52e1370064a55c9163e204da6a4bc8412f5f1c29
Mario Sangiorgio <mariosangiorgio@gmail.com>
2016-12-04 02:28:01 +0000

    Account auto-completion

--- server.ts
+++ server.ts
@@ -1,13 +1,27 @@
 'use strict';
-import {IPCMessageReader, IPCMessageWriter, createConnection, IConnection, TextDocument, TextDocuments, Diagnostic, DiagnosticSeverity, InitializeParams, InitializeResult, NotificationType} from 'vscode-languageserver';
+import {IPCMessageReader, IPCMessageWriter, createConnection, IConnection, TextDocument, TextDocumentSyncKind, TextDocuments, Diagnostic, DiagnosticSeverity, InitializeParams, InitializeResult} from 'vscode-languageserver';
+import {CompletionItem, CompletionItemKind, TextDocumentPositionParams} from 'vscode-languageserver';
 import {Ledger} from 'ledger-cli';
 import * as url from 'url';
 
-let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
-let documents: TextDocuments = new TextDocuments();
+let accounts : string[] = [];
 
+let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+
+connection.onCompletion((textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    return accounts.map((account, index) => {
+        return {
+            label: account,
+            kind: CompletionItemKind.Text,
+            data: index
+        }
+    })
+});
+
+let documents: TextDocuments = new TextDocuments();
 documents.onDidChangeContent((change) =>{
     connection.console.info("Content changed")
+    connection.console.info(change.document.getText())
     //TODO: ditch ledger-cli so issues can be reported in real time
     let diagnostics: Diagnostic[] = [];
     /*
@@ -24,6 +38,11 @@ documents.onDidChangeContent((change) =>{
     */
 })
 
+documents.onDidOpen(params => {
+    let ledger = new Ledger({ file: decodeURI(url.parse(params.document.uri).path) });
+    ledger.accounts().on('data', account => accounts.push(account))
+})
+
 documents.onDidSave((change) => {
     let ledger = new Ledger({ file: decodeURI(url.parse(change.document.uri).path) });
     ledger.stats((err, stat) => {
@@ -34,14 +53,17 @@ documents.onDidSave((change) => {
 });
 
 documents.listen(connection);
-let workspaceRoot: string;
+
 connection.onInitialize((params): InitializeResult => {
-	workspaceRoot = params.rootPath;
 	return {
 		capabilities: {
-			textDocumentSync: documents.syncKind
-			}
-		}
+            //TODO: evaluate if this should be done incrementally
+            textDocumentSync: documents.syncKind,
+            completionProvider: {
+                resolveProvider: false
+            }
+        }
+    }
 });
 
 connection.listen();
