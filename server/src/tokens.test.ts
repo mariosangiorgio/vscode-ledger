1547f1872858f32d35a5305798061b96f7558c04
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-07-20 15:54:50 +0700

    Removed stubs for tokenization. If I'm going to work on it I'll reinstate them

--- server/src/tokens.test.ts
+++
@@ -1,9 +0,0 @@
-import {tokenize} from './tokens';
-import {expect} from 'chai';
-import 'mocha';
-
-describe('Tokenization', () => {
-  it('should work with empty lines', () =>{
-    expect(tokenize('')).to.be.empty;
-  });
-});
